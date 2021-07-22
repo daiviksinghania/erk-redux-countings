8438f366a19be1755bd4c039e563a1db5ab313df
errahulkumar <er.rahulkumar13@gmail.com>
2021-07-22 09:23:05 +0530

    redux saga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import {useSelector, useDispatch } from "react-redux";
 import { counting, countingAsync, reverseCounting } from "./actions/index";
 
@@ -9,7 +9,7 @@ function App() {
     <div className="container">
         <h1>Countings & Backword Countings</h1>
         <h4>Using React & Redux</h4>
-        <div class="countings_container">
+        <div className="countings_container">
           <a className="revcountings" title="Backword Counting"  onClick={ () => dispatch(reverseCounting()) }><span>Backword counting -</span></a>
           <input name="countings_input" type="text" className="countings_input" value={myState}></input>
           <a className="countings" title="countings" onClick={ () => dispatch(counting()) }><span>Counting +</span></a>
